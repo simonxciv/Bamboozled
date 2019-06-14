dd785017b282f325868b31d8dc5a5e2fc43cbad8
Simon Buckley <hello@simonbuckley.com.au>
2019-06-14 15:30:03 +1000

    New feature: Get-BambooHRFields

--- Bamboozled/Bamboozled.psm1
+++ Bamboozled/Bamboozled.psm1
@@ -21,6 +21,42 @@ function Get-BambooHRAuth {
 }
 
 <# ---------------------------------------------------------------------------------------------------------------------------------------
+    GET-BAMBOOHRFIELDS
+--------------------------------------------------------------------------------------------------------------------------------------- #>
+
+function Get-BambooHRFields {
+    param(
+        [Parameter(Mandatory=$true,Position=0)]$ApiKey,
+        [Parameter(Mandatory=$true,Position=1)]$subDomain
+    )
+
+    # Force use of TLS1.2 for compatibility with BambooHR's API server. Powershell on Windows defaults to 1.1, which is unsupported
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+    
+    # API endpoint URL
+    $fieldsUrl = "https://api.bamboohr.com/api/gateway.php/{0}/v1/meta/fields" -f $subDomain
+
+    # Build a BambooHR credential object using the provided API key
+    $bambooHRAuth = Get-BambooHRAuth -ApiKey $apiKey
+
+    # Attempt to connect to the BambooHR API Service
+    try
+    {
+        # Perform the API query
+        $bambooHRFields = Invoke-WebRequest $fieldsUrl -method GET -Credential $bambooHRAuth -Headers @{"accept"="application/json"}
+
+        # Convert the output to a PowerShell object
+        $bambooHRFields = $bambooHRFields.Content | ConvertFrom-Json
+    }
+    catch
+    {
+        throw "Failed to retrieve a list of fields."
+    }
+
+    Return $bambooHRFields
+}
+
+<# ---------------------------------------------------------------------------------------------------------------------------------------
     GET-BAMBOOHRDIRECTORY
 --------------------------------------------------------------------------------------------------------------------------------------- #>
 
@@ -185,4 +221,4 @@ function Get-BambooHRUser {
     return $bambooHRUser
 }
 
-Export-ModuleMember -Function Get-BambooHRDirectory, Get-BambooHRUser
+Export-ModuleMember -Function Get-BambooHRDirectory, Get-BambooHRUser, Get-BambooHRFields
